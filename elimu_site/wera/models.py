24150a15934c24dc1161f3f856cde8210a9101dc
vincenttommi <vincenttommi@gmail.com>
2023-09-27 02:05:20 +0300

    Connecting data and admin panel

--- elimu_site/wera/models.py
+++ elimu_site/wera/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+#creating our tables inside here
+
+class Room(models.Model):
+    #host = 
+    #topic=
+    name  = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    #participants
+    updated = models.DateField(auto_now=True)
+    created  = models.DateField(auto_now_add=True)
+
+
+    def  __str__(self):
+        return self.name
+    #converting an object of the class to a string that returns name
+    #as the attribute
