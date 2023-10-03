169978fccd33179ce1fdb2f4b980ac5ddcad2c2b
vincenttommi <vincenttommi@gmail.com>
2023-10-03 12:15:28 +0300

    adding updating route method

--- elimu_site/wera/urls.py
+++ elimu_site/wera/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('room/<str:pk>/', views.room, name="room"),
     path('scores/', views.scores, name="scores"),
     path('navbar/', views.navbar,name="navbar" ),
-    path('create-room/',views.createRoom, name="create-room")
+    path('create-room/',views.createRoom, name="create-room"),
+    path('update-room/<str:pk>/', views.updateRoom, name = "update-room")
   
 
 ]
--- elimu_site/wera/views.py
+++ elimu_site/wera/views.py
@@ -41,3 +41,26 @@ def createRoom(request):
             
         context = {'form': form}
         return render(request, 'wera/room_form.html', context) #used to render the template       
+
+
+
+#In summary, this view function is used to display a form for updating
+# a specific Room object. It retrieves the existing room data, 
+# creates a form instance pre-filled with that data, 
+# and renders a template to display the form to the user for editing and updating.
+def  updateRoom(request,pk):
+
+    room  = Room.objects.get(id=pk) #retrieves a specific Room object from the database using it's primary key
+    form  = RoomForm(instance=room) # create  aform instance  for  the retrieved Room object (room)
+    
+    #checking if the   data is posted and being redirected
+    if request.method  == 'POST':
+        form  = RoomForm(request.POST,instance=room)
+        if  form.is_valid():
+            form.save()
+            return  redirect('home')
+    
+    
+    
+    context = {'form':form} #  a form dictionary containing it's'form' variable
+    return render(request, 'wera/room_form.html', context)
