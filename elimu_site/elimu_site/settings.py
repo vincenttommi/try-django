8db405e689c09e643601818da99056b92dde2b98
vincenttommi <vincenttommi@gmail.com>
2023-10-31 15:40:03 +0300

    example of fetching

--- elimu_site/elimu_site/settings.py
+++ elimu_site/elimu_site/settings.py
@@ -39,9 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'wera.apps.WeraConfig',
     'rest_framework',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,3 +140,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOW_ALL_ORIGINS  = True
+#allows all our url to access our website
